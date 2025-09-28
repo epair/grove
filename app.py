5d9dd6a52b0790250016e71de8990a96ef5e9768
Emmett Pair <emmett.pair@postie.com>
2025-09-28 15:12:02 -0400

    Setup basic layout

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Static, ListView, ListItem, Label
 
+TEXT = """\
+Docking a widget removes it from the layout and fixes its position, aligned to either the top, right, bottom, or left edges of a container.
+
+Docked widgets will not scroll out of view, making them ideal for sticky headers, footers, and sidebars.
+
+"""
+
+class Sidebar(ListView):
+    def compose(self) -> ComposeResult:
+        yield ListItem(Label("One"))
+        yield ListItem(Label("Two"))
+        yield ListItem(Label("Three"))
 
 class GroveApp(App):
     """A Textual app to manage git worktrees."""
 
+    CSS_PATH = "app.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header()
+        yield Sidebar(id='sidebar')
+        yield Static(TEXT * 10, id="body")
         yield Footer()
 
     def action_toggle_dark(self) -> None:
